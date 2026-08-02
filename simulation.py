5e02d6e0044a841697d0fac233b6911aa418fcb3
zuorenchen <zuorenchen@m110.nthu.edu.tw>
2026-08-02 19:49:52 +0100

    Update launch site

--- simulation.py
+++ simulation.py
@@ -22,17 +22,16 @@ def main():
 
     # Create environment
     env = Environment(
-        gravity=9.80665,
-        date=(2023, 10, 13, 14),
-        latitude=39.388692,
-        longitude=-8.287814,
-        elevation=130,
+        gravity=9.78825,  # 大劉南/Q037/一等水準點 內政部103年公告二等重力點測量成果(計劃年度：2014) 全國衛星追蹤站暨基本控制點查詢系統
+        date=(2026, 8, 1, 7),  # 15:00 local time
+        latitude=22.17492027,  # 國家科學及技術委員會短期科研探空火箭發射場域
+        longitude=120.8926564,
+        elevation=31.9331,
         datum="WGS84",
-        timezone="Portugal",
+        timezone="UTC",
+        max_expected_height=10000,
     )
-
     env.set_atmospheric_model(type="Windy", file="ECMWF")
-    env.max_expected_height = 4000
     env_plot_path = output_path + "env.png"
     env.plots.info(filename=env_plot_path)
 
